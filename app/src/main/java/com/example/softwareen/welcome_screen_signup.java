3d3e2619e51a08ee60b4baaaf9c39506ac984f05
Ben MacNeil <bn524345@dal.ca>
2020-02-09 15:24:35 -0400

    added login screen added signup screen added ability to go between login -> signup

---
+++ app/src/main/java/com/example/softwareen/welcome_screen_signup.java
@@ -0,0 +1,14 @@
+package com.example.softwareen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class welcome_screen_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen_signup);
+    }
+}
