5f6ed5043bc9d09d4285daced1e4e140cc66b1dc
Joshua C <js530711@dal.ca>
2020-02-11 15:45:48 -0400

    Cleaned up the stubs for testing.
    -Joshua and Robert

---
+++ app/src/androidTest/java/com/example/softwareen/espresso_test.java
@@ -0,0 +1,94 @@
+package com.example.softwareen;
+
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.Rule;
+
+import org.junit.runner.RunWith;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+
+public class espresso_test {
+
+    @Rule
+    public ActivityScenarioRule<welcome_screen_login> activityScenarioRule
+            = new ActivityScenarioRule<>(welcome_screen_login.class);
+
+    /* USER STORY 1 TESTS */
+
+    @Test
+    public void sign_up_empty_email_feild() {
+
+
+    }
+
+    @Test
+    public void sign_up_empty_password_feild() {
+
+
+    }
+
+    @Test
+    public void sign_up_invalid_email() {
+
+
+    }
+
+    @Test
+    public void sign_up_invalid_password() {
+
+
+    }
+
+    @Test
+    public void correct_sign_up_input_name_and_email() {
+
+    }
+
+    /* USER STORY 2 TESTS */
+
+    @Test
+    public void sign_in_email_incorrect() {
+
+    }
+
+    @Test
+    public void sign_in_password_incorrect() {
+
+
+    }
+
+    @Test
+    public void sign_in_empty_email_feild() {
+
+
+    }
+
+    @Test
+    public void sign_in_empty_name_feild() {
+
+
+    }
+
+    @Test
+    public void correct_sign_in_input_name_and_email() {}
+
+}
+
+
+
