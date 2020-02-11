1dd4771048b4143f178d13621c188520b24658d8
srahman <sm448201@dal.ca>
2020-02-11 16:48:05 -0400

    Home screen add button created that is linked to add_substance_list class.COuld not test as app crashes after trying to log in. Worked by Mahir Labib Iqbal and Asif Rahman

--- app/src/main/java/com/example/softwareen/home_screen.java
+++ app/src/main/java/com/example/softwareen/home_screen.java
@@ -2,13 +2,38 @@ package com.example.softwareen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home_screen extends AppCompatActivity {
+
+    private Button butt;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+//Thats is the button reference ID
+        butt=(Button)findViewById(R.id.addSubstance_button);
+        /*This sets up the screen so that when the button is clicked, it automatically goes to the
+       secondActivity which is the add_substance_list.class */
+        butt.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                action();
+            }
+
+   });
+        }
+    public void action()
+    {
+        Intent i=new Intent(home_screen.this,add_substance_list.class);
+        startActivity(i);
     }
 }
